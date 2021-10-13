136ec077cfba9671791cd2b2f8574dc8912ace4b
mani-osahan <manipaloshan@gmail.com>
2021-10-12 20:11:52 -0400

    initial push and commit to start up repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+       <title>Manipal Osahan</title>
+     </header>
+
+      <h1>Manipal Osahan</h1>
+    </div>
+  );
+}
+
+export default App;
